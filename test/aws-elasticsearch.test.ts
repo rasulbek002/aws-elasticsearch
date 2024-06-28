0b5f2453bd690c1040170e5edd60e31f16922d0c
Rasul Karimov <rasul@mancho.dev>
2024-06-28 16:29:57 +0600

    Initial commit

---
+++ test/aws-elasticsearch.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsElasticsearch from '../lib/aws-elasticsearch-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-elasticsearch-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsElasticsearch.AwsElasticsearchStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
